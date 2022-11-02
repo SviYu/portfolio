5f949ce22bc6faa4dd20d181d005b39ee7e2e444
Svitlana Yukhymenko <104594964+SviYu@users.noreply.github.com>
2022-11-02 10:16:06 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+/* Scrolling animation */
+window.onscroll = function() { myFunction() };
+
+function myFunction() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById('myBar').style.width = scrolled + '%'
+}
+
+/* Modal CV */
+let modal = document.getElementById('myModal');
+let imgView = document.querySelector('.view');
+let modalImg = document.getElementById('img01');
+let cvImg = document.querySelector('.cv-bgPhoto');
+
+imgView.onclick = function () {
+    modal.style.display = 'block';
+    modalImg.src = cvImg.src;
+}
+let close = document.getElementsByClassName('close')[0];
+close.onclick = function () {
+    modal.style.display = 'none';
+}
+
+/* Burger menu animation */
+const burgerMenu = document.getElementById('burgerMenu');
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+})
+
+/* Burger menu */
+let mobileMenu = document.querySelector(".burger-menu-mobile");
+
+function toggleMenu() {
+  if (mobileMenu.style.display === "block") {
+    mobileMenu.style.display = "none";
+  } else {
+    mobileMenu.style.display = "block";
+    };
+}
+
+/* Menu closes automatically after clicking links */
+let menuLinks = document.querySelectorAll('.mobile-list-item');
+menuLinks.forEach(el => el.addEventListener('click', function (e) {
+    mobileMenu.style.display = 'none';
+}));
+
+/* Preloader */
+let loader = document.getElementById('preloader');
+window.addEventListener('load', () => {
+    loader.style.display = 'none'
+})
+
+
